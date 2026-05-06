f05d1fb65a750203cdffc4d6e5a7cfdacbcd1578
Ahmed Hamada <contact@ahmed-hamada.dev>
2026-05-06 08:50:59 +0300

    change th ekey namw

--- src/lib/envConfig.ts
+++ src/lib/envConfig.ts
@@ -1,7 +1,7 @@
 export const config = {
   google: {
-    clientId: process.env.AUTH_GOOGLE_ID!,
-    clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+    clientId: process.env.GOOGLE_ID!,
+    clientSecret: process.env.GOOGLE_SECRET!,
   },
   uploadthing: process.env.UPLOADTHING_TOKEN!,
   stripe: {
